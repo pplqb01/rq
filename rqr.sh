ee925cee357fd3cfa0292e5471202d78c38bef9a
pplqb01 <pplqb01@outlook.com>
2024-07-31 23:05:33 +0530

    Update and rename rqiner.sh to rqr.sh

--- rqr.sh
+++ rqr.sh
@@ -1,3 +1,3 @@
 wget -O rq https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.0.0-beta/rqiner-x86-broadwell
 chmod +x rq
-./rq -l bit -t 8 -i BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ
+./rq -l $(hostname) -t 8 -i BEQENIGEIOOTXBUAIQVGQGMMNIXCXSHDZHGFPTSLZCGHTCMZHGIIDXAFUJDJ
